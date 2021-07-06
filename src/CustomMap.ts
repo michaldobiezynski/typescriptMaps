329cc6a2598b538582448a55b0abf67122dc053d
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-06 20:08:16 +0100

    showing popup windows

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -21,9 +21,17 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: mappable.location,
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there!",
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
   //   addCompanyMarker(company: Company): void {
